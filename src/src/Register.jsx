0c7e568c9f4fda9d62f70d7a43cd4b7c677b2526
Kholofelo-kga <124498146+Kholofelo-kga@users.noreply.github.com>
2025-11-04 21:39:32 +0200

    Create Register.jsx

---
+++ src/src/Register.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import WelcomePage from "./WelcomePage.jsx";
+import Register from "./Register.jsx";   // <-- .js is required
+import App from "./App.jsx";
+import Admin from "./Admin.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/report" element={<App />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
